b240add6f3f11838d8002d01d5d3377c70ec8ffa
Dream <166921709+hamoudi223@users.noreply.github.com>
2026-07-14 22:30:15 +0000

    Delete _stickers.js

--- _stickers.js
+++
@@ -1,7 +0,0 @@
-// api/_stickers.js
-
-export const STICKERS = [
-  "https://i.ibb.co/3Y46g4s/toxic-clown.webp", // Sticker clown (pour les autres)
-  "https://i.ibb.co/r7X0vX6/toxic-skull.webp",  // Sticker skull (pour les autres)
-  "https://i.ibb.co/vXqfD3D/toxic-love.webp"    // Un sticker mignon/respectueux (quand le Maître parle !)
-];
